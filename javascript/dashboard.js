45560292934d413a7e83a2a4ce6601014c1057ce
Davron <davronbekq467@gmail.com>
2022-07-30 20:27:21 +0500

    calculator page done

---
+++ javascript/dashboard.js
@@ -0,0 +1,33 @@
+const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45, 50, 55, 40, 35, 30],
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {},
+};
+const myChart = new Chart(document.getElementById("myChart"), config);
