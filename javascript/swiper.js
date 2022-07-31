1ea028e9778b08d4f8caa78968b47071cf30314a
Davron <davronbekq467@gmail.com>
2022-07-31 16:05:18 +0500

    News, Contact, Cart pages done

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -31,6 +31,30 @@ var swiper = new Swiper(".mySwiper", {
 
 // index-page-product-card
 
+var swiper = new Swiper(".index-page-product", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
 var swiper = new Swiper(".clinet_brand", {
   slidesPerView: 4,
   spaceBetween: 30,
@@ -52,5 +76,24 @@ var swiper = new Swiper(".clinet_brand", {
     1200: {
       slidesPerView: 4,
     },
+  },
+});
+
+// Product page
+
+var swiper = new Swiper(".product_img", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
   },
 });
