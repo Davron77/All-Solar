5e115adff2ee886941b8d272af6bba6a33a811b8
Davron <davronbekq467@gmail.com>
2022-07-31 23:01:29 +0500

    vacansies page done

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -18,8 +18,8 @@ const data = {
   datasets: [
     {
       label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "#00b533",
+      borderColor: "#00b533",
       data: [0, 10, 5, 2, 20, 30, 45, 50, 55, 40, 35, 30],
     },
   ],
